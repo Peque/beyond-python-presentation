3bce36a530806b7368a6c84f0d0fc4a40edd3e55
Miguel Sánchez de León Peque <peque@neosit.es>
2019-10-04 11:15:31 +0200

    Update pytest-pdb code example

--- code/pytest-pdb/my_module.py
+++ code/pytest-pdb/my_module.py
@@ -2,4 +2,5 @@ import requests
 
 
 def get_ip():
+    """Get my current external IP."""
     return requests.get("https://icanhazip.com")
--- code/pytest-pdb/mymodule.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-
-def get_ip():
-    """Get my current external IP."""
-    return requests.get("https://icanhazip.com")
